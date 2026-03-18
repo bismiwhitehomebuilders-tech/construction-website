1faf99c34ff887253f07432c0a48cd781d5885fc
Bismi White Home Builders <yourgithubemail.com>
2026-03-18 19:07:49 +0530

    fixed toggle

--- script.js
+++ script.js
@@ -3,7 +3,8 @@ document.queryselector(".menu-toggle");
 const nav =
 document.queryselector(".nav-links");
 
+if(toggle){
 toggle.addEventListener("click", () => {
 nav.classList.toggle("active");
 });
-
+}
